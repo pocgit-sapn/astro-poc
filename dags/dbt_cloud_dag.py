41fa0120bf005400f080eaebdaea0dd094232073
pocgit-sapn <pocgit20241014@outlook.com>
2024-10-29 15:00:34 +1030

    Update dbt_cloud_dag.py

--- dags/dbt_cloud_dag.py
+++ dags/dbt_cloud_dag.py
@@ -12,9 +12,11 @@ import pendulum
     start_date=pendulum.datetime(2023, 1, 1),
     catchup=False,
      default_args={
+         "retries":0,
+        "dbt_cloud_conn_id":"dbt_conn",
     },   
  )
-def dbt_cloud_dag()
+def dbt_cloud_dag():
     dbt_cloud_list_jobs = DbtCloudListJobsOperator(
         task_id="dbt_cloud_list_jobs",
     project_id="9157",
